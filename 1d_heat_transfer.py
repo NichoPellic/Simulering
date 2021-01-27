2da59d294130a94a3f347597b81c0b2d12e53717
Nicholas Lislien Baekken <nicholb@live.com>
2021-01-27 17:43:59 +0100

    first commit

---
+++ 1d_heat_transfer.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib as plt
+
+delta_x = 1     #Each step
+alpha = 1       #Coefficient
+
+inital_value = 100
+left_boundry = 0
+rigth_boundry = 0
+
+iterations = 100
+
+x_axis = 100    #Aka lenght
+
+nodes = np.linespace(0, x_axis)
+
+u_array = np.empty(iterations, nodes)
+
+def calculate(u):
+    for k in range(iterations + 1):        
+        for i in range(nodes + 1): #Maybe not neccessary ( + 1)
+            u[k + 1, i] = u[k, i] + alpha((u[k, i+1] - 2(u[k, i]) + u[k, i-1])/delta_x**2) 
+    return u
+
+def plot():
